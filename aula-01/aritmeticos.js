35d0c7fbde3f36dd9a8e470b065561d82bfca753
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-20 12:34:28 -0300

    (Aula 01) Implementa operações aritméticas básicas
    Adiciona funcionalidades de soma, subtração, multiplicação, divisão, módulo, exponenciação e cálculo de raiz quadrada.

---
+++ aula-01/aritmeticos.js
@@ -0,0 +1,42 @@
+//* VALORES ARITMÉTICOS
+//* SOMA
+let numero1 = 68;
+let numero2 = 88;
+let resultadoSoma = numero1 + numero2;
+// console.log(resultadoSoma);
+
+//* SUBTRAÇÃO
+let numero3 = 68;
+let numero4 = 88;
+let resultadoSub = numero4 - numero3;
+// console.log(resultadoSub);
+
+//* MULTIPLICAÇÃO
+let numero5 = 60;
+let numero6 = 80;
+let resultadoMul = numero5 * numero6;
+// console.log(resultadoMul);
+
+//* DIVISÃO
+let numero7 = 60;
+let numero8 = 80;
+let resultadoDiv = numero7 / numero8;
+// console.log(resultadoDiv);
+
+//* MÓDULO
+let resto = 100 % 3; // 1
+// 100 / 3 = 33.33
+// 1 x 33.33
+// 2 x 33.33
+// 3 x 33.33
+// console.log(resto);
+
+//* EXPONENCIAÇÃO
+let multi = 2 ** 3; // 8
+// console.log(multi);
+let multi2 = Math.pow(2,3); // 8
+console.log(multi2);
+
+//* RAIZ QUADRADA
+let raiz = Math.sqrt(144); // 12
+// console.log(raiz)
