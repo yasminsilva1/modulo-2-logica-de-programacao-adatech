6134cfbf3846ba72631e4937648ccb53b77318bd
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-20 13:01:41 -0300

    (Aula 05) Adiciona exemplos de estrutura de controle de repetição
    Inclui estruturas de controle de repetição com while e do while.

---
+++ aula-05/while.js
@@ -0,0 +1,30 @@
+//* estrutura de controle para criar loops
+//* while, do while
+
+//* while valida antes da execução
+// while (condicao) {
+//   // codigo que vai ser executado
+// }
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+//* do while executa o bloco ao menos 1 vez
+// do {
+//   // codigo a ser executado
+// } while (condicao);
+
+// let i = 0;
+// do {
+// 	console.log(`mensagem ${i}`);
+// 	i++;
+// } while (i < 5);
+
+// let numero;
+// do {
+// 	numero = Math.floor(Math.random() * 100);
+//   console.log(numero);
+// } while (numero % 5 === 0);
