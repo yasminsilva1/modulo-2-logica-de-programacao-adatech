383af790097fce870f2dd9c6c863034667b63b94
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-20 12:56:17 -0300

    (Aula 04) Adiciona exemplos de operadores lógicos e de comparação

---
+++ aula-04/logica.js
@@ -0,0 +1,49 @@
+//* && AND
+//* || OR
+//* ! NOT
+
+//* > maior que
+//* < menor que
+//* >= maior ou igual
+//* <= menor ou igual
+//* == igual - valor
+//* === estritamente igual - valor e tipo
+//* != diferente
+//* !== estritamente diferente
+
+let numero1 = "10";
+let numero2 = 10;
+
+// if(numero1 === numero2){
+//     console.log('verdade')
+// }else{
+//     console.log('falso')
+// }
+
+// console.log(false && true); // false
+// console.log(!false && !true); // false
+
+const a = 3;
+const b = -2;
+// console.log(a > 0 || !b < 0) // true
+
+// console.log(false || (false && true)); // false
+
+const valor = 2 === 3 || (4 < 0 && 1 === 1); 
+// console.log(valor); // false
+
+// let validacao = null // falso
+// let validacao = NaN // falso
+// let validacao = undefined // falso
+// let validacao = "" // falso
+// let validacao = "" //verdadeiro
+
+let validacao;
+validacao = 0;
+
+//console.log(validacao);
+// if (validacao) {
+// 	console.log("verdadeiro");
+// } else {
+// 	console.log("falso");
+// }
