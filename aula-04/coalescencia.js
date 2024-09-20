866a273c5154e0635e2d06d145fe016415ef26e6
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-20 12:57:44 -0300

    (Aula 04) Adiciona exemplos de uso do operador de coalescência nula

---
+++ aula-04/coalescencia.js
@@ -0,0 +1,8 @@
+const teste = null ?? "default string";
+// console.log(teste);
+
+let input = "Ola Everton da silva, Seja Bem vindo";
+// input = undefined
+
+const mensagem = input ?? "Ola visitante, Seja bem vindo";
+// console.log(mensagem);
