141a102e15851f84ace63cf703ce22aee2c99e19
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-20 13:03:35 -0300

    (Aula 05) Apresenta o conceito de truthy e falsy
    Inclui exemplos de valores que são considerados truthy e falsy.

---
+++ aula-05/truthy-falsy.js
@@ -0,0 +1,55 @@
+//* Truthy - são tratados como verdadeiros quando o contexto boolean
+//* Flasy - são tratados como falso quando o contexto boolean
+
+//* Valores não vazios em geral são verdadeiros / true
+
+//* string
+// if ("Olá mundo") {
+// 	console.log("Verdadeiro");
+// }
+
+//* valores numericos
+//* valores verdadeiros - valores maiores que 0 e menores que 0.
+
+//* true = 1
+//* false = 0
+
+// if (-42) {
+// 	console.log("Verdadeiro");
+// }
+
+//* VALORES FALSOS
+// null, undefined, NaN, "", 0, false
+
+// let variavelNaoDefinida;
+// if (variavelNaoDefinida) {
+// 	console.log("Verdadeiro");
+// } else {
+// 	console.log("Falso");
+// }
+
+// let variavelNaoDefinida = 10 * "texto";
+// if (variavelNaoDefinida) {
+// 	console.log("Verdadeiro");
+// } else {
+// 	console.log("Falso");
+// }
+
+// let variavelNaoDefinida = 10 * "texto";
+// if (isNaN(variavelNaoDefinida)) {
+// 	console.log("Verdadeiro");
+// }
+
+// let variavelArray = [];
+// if (variavelArray) {
+// 	console.log("Verdadeiro");
+// } else {
+// 	console.log("Falso");
+// }
+
+// let variavelObj = {};
+// if (variavelObj) {
+// 	console.log("Verdadeiro");
+// } else {
+// 	console.log("Falso");
+// }
